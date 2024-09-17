fe5a18977ee22c2502e1c0684246d34c87cd6783
Aaron Krishna <aaron.krishna@laysholdings.co.za>
2024-09-17 10:52:46 +0200

    added more builder patterns

---
+++ StepwiseBuilder/Interfaces/ISpecifyCarType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepwiseBuilder.Interfaces
+{
+    public interface ISpecifyCarType
+    {
+        public ISpecifyWheelSize OfType(CarType type);
+    }
+
+}
